bfa577732cc3ef08486bf115c246bbc76220bde5
prit4848 <pritrajput415@gmail.com>
2024-09-29 13:34:52 +0530

    initial commit

---
+++ config/mongoose-connection.js
@@ -0,0 +1,19 @@
+
+
+const dbgr = require("debug")("development:mongoose")
+
+
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose
+  .connect(`${process.env.Mongodb_uri}/webapp`)
+  .then(() => {
+    dbgr("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+// Export the mongoose connection
+module.exports = mongoose.connection;
